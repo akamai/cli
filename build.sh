a2e9a279c3fba0f0ed3870d1ffffd5ac8004e828
Davey Shafik <me@daveyshafik.com>
2017-08-10 15:37:37 -0700

    Output build files into the "build" directory

--- build.sh
+++ build.sh
@@ -17,13 +17,15 @@ fi
 
 check_version $1
 
-GOOS=darwin GOARCH=amd64 go build -o akamai-$1-macamd64 .
-shasum -a 256 akamai-$1-macamd64 | awk '{print $1}' > akamai-$1-macamd64.sig
-GOOS=linux GOARCH=amd64 go build -o akamai-$1-linuxamd64 .
-shasum -a 256 akamai-$1-linuxamd64 | awk '{print $1}' > akamai-$1-linuxamd64.sig
-GOOS=linux GOARCH=386 go build -o akamai-$1-linux386 .
-shasum -a 256 akamai-$1-linux386 | awk '{print $1}' > akamai-$1-linux386.sig
-GOOS=windows GOARCH=386 go build -o akamai-$1-windows386.exe .
-shasum -a 256 akamai-$1-windows386.exe | awk '{print $1}' > akamai-$1-windows386.exe.sig
-GOOS=windows GOARCH=amd64 go build -o akamai-$1-windowsamd64.exe .
-shasum -a 256 akamai-$1-windowsamd64.exe | awk '{print $1}' > akamai-$1-windowsamd64.exe.sig
+mkdir -p build
+
+GOOS=darwin GOARCH=amd64 go build -o build/akamai-$1-macamd64 .
+shasum -a 256 build/akamai-$1-macamd64 | awk '{print $1}' > build/akamai-$1-macamd64.sig
+GOOS=linux GOARCH=amd64 go build -o build/akamai-$1-linuxamd64 .
+shasum -a 256 build/akamai-$1-linuxamd64 | awk '{print $1}' > build/akamai-$1-linuxamd64.sig
+GOOS=linux GOARCH=386 go build -o build/akamai-$1-linux386 .
+shasum -a 256 build/akamai-$1-linux386 | awk '{print $1}' > build/akamai-$1-linux386.sig
+GOOS=windows GOARCH=386 go build -o build/akamai-$1-windows386.exe .
+shasum -a 256 build/akamai-$1-windows386.exe | awk '{print $1}' > build/akamai-$1-windows386.exe.sig
+GOOS=windows GOARCH=amd64 go build -o build/akamai-$1-windowsamd64.exe .
+shasum -a 256 build/akamai-$1-windowsamd64.exe | awk '{print $1}' > build/akamai-$1-windowsamd64.exe.sig
