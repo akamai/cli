f1b3da49122480928000fac37fe4d31dbc7dbef8
Davey Shafik <me@daveyshafik.com>
2018-02-09 15:45:12 -0800

    Use `colorable.NewColorable*()` to fix colored output on Windows
    Fixes #35

--- akamai.go
+++ akamai.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/mattn/go-colorable"
 	"github.com/urfave/cli"
 )
 
@@ -40,6 +41,8 @@ func main() {
 	app.Usage = "Akamai CLI"
 	app.Version = VERSION
 	app.Copyright = "Copyright (C) Akamai Technologies, Inc"
+	app.Writer = colorable.NewColorableStdout()
+	app.ErrWriter = colorable.NewColorableStderr()
 
 	firstRun()
 
