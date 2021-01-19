4f54d925da94963bb3408dc777b1eb8aa3b22174
Piotrowski, Piotr <pipiotro@akamai.com>
2021-01-19 13:47:39 +0100

    [TFP-543] Add setting log output with env

--- pkg/log/logv2.go
+++ pkg/log/logv2.go
@@ -2,7 +2,6 @@ package log
 
 import (
 	"context"
-	"fmt"
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/text"
 	"github.com/urfave/cli/v2"
@@ -13,14 +12,22 @@ func SetupContext(ctx context.Context, app *cli.App) context.Context {
 	logger := &log.Logger{
 		Level: log.InfoLevel,
 	}
+	output := app.Writer
+	if outputEnv := os.Getenv("AKAMAI_LOG_PATH"); outputEnv != "" {
+		f, err := os.OpenFile(outputEnv, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Warn("Invalid value of AKAMAI_LOG_PATH")
+		}
+		output = f
+	}
 	if lvlEnv := os.Getenv("AKAMAI_LOG"); lvlEnv != "" {
 		logLevel, err := log.ParseLevel(lvlEnv)
 		if err == nil {
 			logger.Level = logLevel
 		} else {
-			fmt.Fprintln(app.Writer, "[WARN] Unknown AKAMAI_LOG value. Allowed values: panic, fatal, error, warn, info, debug, trace")
+			log.Warn("Unknown AKAMAI_LOG value. Allowed values: panic, fatal, error, warn, info, debug, trace")
 		}
 	}
-	logger.Handler = text.New(app.Writer)
+	logger.Handler = text.New(output)
 	return log.NewContext(ctx, logger)
 }
