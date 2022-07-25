4e94d62d8246b017f01566c2faee7c090a98e3e7
Tatiana Slonimskaia <tslonims@akamai.com>
2022-07-25 15:37:48 +0200

    DXE-1372 Fixed help template for sub-commands

--- pkg/apphelp/help_command_test.go
+++ pkg/apphelp/help_command_test.go
@@ -156,7 +156,10 @@ Name:
   apphelp.test%s test - A new cli application
 
 Usage:
-  apphelp.test%s [global flags] test [command flags] <subcommand> 
+  apphelp.test%s [global flags] test [command flags] <subcommand>
+
+Description:
+  test command
 
 Subcommands:
   subcommand-no-category
@@ -166,6 +169,105 @@ category1:
   subcommand-in-category1
 category2:
   subcommand-in-category2
+
+Command Flags:
+  --help, -h  show help (default: false)
+
+Global Flags:
+  --edgerc value, -e value   edgerc config path passed to executed commands, defaults to ~/.edgerc
+  --section value, -s value  edgerc section name passed to executed commands, defaults to 'default'
+`, binarySuffix, binarySuffix),
+		},
+
+		"help for subcommand no category": {
+			args: []string{"test", "subcommand-no-category"},
+			cmd: &cli.Command{
+				Name:        "test",
+				Description: "test command",
+				Category:    "",
+				Action:      func(ctx *cli.Context) error { fmt.Println("oops!"); return nil },
+				Subcommands: []*cli.Command{
+					{
+						Name:        "subcommand-no-category",
+						Description: "a test subcommand without a category",
+					},
+					{
+						Name:        "subcommand-with-aliases",
+						Description: "a test subcommand with aliases and without a category",
+						Aliases:     []string{"sub-wa", "s-w-a"},
+					},
+					{
+						Name:        "subcommand-in-category1",
+						Description: "a test subcommand in category 1",
+						Category:    "category1",
+					},
+					{
+						Name:        "subcommand-in-category2",
+						Description: "a test subcommand in category 2",
+						Category:    "category2",
+					},
+				},
+			},
+			expectedOutput: fmt.Sprintf(`
+Name:
+  apphelp.test%s test subcommand-no-category
+
+Usage:
+  apphelp.test%s [global flags] test subcommand-no-category [command flags] 
+
+Description:
+  a test subcommand without a category
+
+Command Flags:
+  --help, -h  show help (default: false)
+
+Global Flags:
+  --edgerc value, -e value   edgerc config path passed to executed commands, defaults to ~/.edgerc
+  --section value, -s value  edgerc section name passed to executed commands, defaults to 'default'
+`, binarySuffix, binarySuffix),
+		},
+
+		"help for subcommands with category": {
+			args: []string{"test", "subcommand-in-category1"},
+			cmd: &cli.Command{
+				Name:        "test",
+				Description: "test command",
+				Category:    "",
+				Action:      func(ctx *cli.Context) error { fmt.Println("oops!"); return nil },
+				Subcommands: []*cli.Command{
+					{
+						Name:        "subcommand-no-category",
+						Description: "a test subcommand without a category",
+					},
+					{
+						Name:        "subcommand-with-aliases",
+						Description: "a test subcommand with aliases and without a category",
+						Aliases:     []string{"sub-wa", "s-w-a"},
+					},
+					{
+						Name:        "subcommand-in-category1",
+						Description: "a test subcommand in category 1",
+						Category:    "category1",
+					},
+					{
+						Name:        "subcommand-in-category2",
+						Description: "a test subcommand in category 2",
+						Category:    "category2",
+					},
+				},
+			},
+			expectedOutput: fmt.Sprintf(`
+Name:
+  apphelp.test%s test subcommand-in-category1
+
+Usage:
+  apphelp.test%s [global flags] test subcommand-in-category1 [command flags] 
+
+Type:
+  category1
+
+Description:
+  a test subcommand in category 1
 
 Command Flags:
   --help, -h  show help (default: false)
